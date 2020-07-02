9b5c943ccf17f021bed26c0df3fd5ab427e4763d
mbuyankin <mbuyankin@gmail.com>
2020-07-02 21:02:35 +0300

    New tasks

---
+++ Problems/Popping the losers/task.py
@@ -0,0 +1,8 @@
+# the following line reads a dict from the input and converts it into an OrderedDict, do not modify it, please
+firms = OrderedDict(json.loads(input()))
+
+# your code here
+firms.popitem(last=True)
+firms.popitem(last=True)
+
+print(firms)
